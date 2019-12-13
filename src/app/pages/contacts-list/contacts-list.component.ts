2ccb8db0adf5ed7ebb32c3235312dca79dbfc1b3
harry <harrytreble@hotmail.co.uk>
2019-12-13 14:24:53 +0800

    Create modal for editing and adding new contacts

--- src/app/pages/contacts-list/contacts-list.component.ts
+++ src/app/pages/contacts-list/contacts-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.scss']
 })
-export class ContactsListComponent implements OnInit {
+export class ContactsListComponent {
+
+  @ViewChild(EditContactComponent, { static: false }) editContactModal: EditContactComponent;
 
   constructor() { }
 
-  ngOnInit() {
+  openEditContactModal(): void {
+    this.editContactModal.openModal();
   }
-
 }
--- src/app/pages/contacts-list/edit-contact/edit-contact.component.ts
+++ src/app/pages/contacts-list/edit-contact/edit-contact.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edit-contact',
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.scss']
 })
-export class EditContactComponent implements OnInit {
+export class EditContactComponent {
+
+  public modalShowing = false;
 
   constructor() { }
 
-  ngOnInit() {
+  openModal(): void {
+    this.modalShowing = true;
+  }
+
+  closeModal(): void {
+    this.modalShowing = false;
+  }
+
+  modalClicked(ev: MouseEvent): void {
+    ev.stopPropagation();
+    ev.preventDefault();
   }
 
 }
