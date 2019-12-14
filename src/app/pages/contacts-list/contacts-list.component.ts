6c2c4d8e3a486656e0d98c01237e099cf2836ffc
harry <harrytreble@hotmail.co.uk>
2019-12-14 16:33:42 +0800

    Displaying the contacts on the contact-list page

--- src/app/pages/contacts-list/contacts-list.component.ts
+++ src/app/pages/contacts-list/contacts-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ContactService, Contact } from 'src/app/services/contact.service';
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.scss']
 })
-export class ContactsListComponent {
+export class ContactsListComponent implements OnInit {
 
   @ViewChild(EditContactComponent, { static: false }) editContactModal: EditContactComponent;
+  public allContacts: Contact[];
 
-  constructor() { }
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit(): void {
+    this.allContacts = this.contactService.getContacts();
+  }
 
   openEditContactModal(): void {
     this.editContactModal.openModal();
