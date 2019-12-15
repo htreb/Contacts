79583c8726fd0d97dec4573b0f4d151308bc28f5
harry <harrytreble@hotmail.co.uk>
2019-12-15 15:08:05 +0800

    Add search bar to filter the contacts

--- src/app/pages/contacts-list/contacts-list.component.ts
+++ src/app/pages/contacts-list/contacts-list.component.ts
@@ -1,16 +1,51 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { EditContactComponent } from './edit-contact/edit-contact.component';
 import { ContactService, Contact } from 'src/app/services/contact.service';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-contacts-list',
   templateUrl: './contacts-list.component.html',
   styleUrls: ['./contacts-list.component.scss']
 })
-export class ContactsListComponent {
+export class ContactsListComponent implements OnInit {
+  @ViewChild(EditContactComponent, { static: false })
+  editContactModal: EditContactComponent;
+  private searchTerm$ = new BehaviorSubject<string>('');
+  public filteredContacts$: Observable<Contact[]>;
+  public favoriteContacts$: Observable<Contact[]>;
 
-  @ViewChild(EditContactComponent, { static: false }) editContactModal: EditContactComponent;
+  constructor(public contactService: ContactService) {}
 
-  constructor(public contactService: ContactService) { }
+  ngOnInit() {
+    this.filteredContacts$ = combineLatest([
+      this.contactService.allContacts$,
+      this.searchTerm$
+    ]).pipe(
+      map(([allContacts, searchTerm]) => {
+        // By stringifying the contact we can easily search all fields: name, email, phone... in one search;
+        return allContacts.filter(
+          c =>
+            JSON.stringify(c)
+              .toLowerCase()
+              .indexOf(searchTerm.toLowerCase()) > -1
+        );
+      })
+    );
+    this.favoriteContacts$ = this.filteredContacts$.pipe(
+      map((orderedContacts: Contact[]) => orderedContacts.filter(c => c.isFavorite))
+    );
+  }
+
+  updateSearchTerm(searchTerm: string) {
+    this.searchTerm$.next(searchTerm);
+  }
+
+  clearSearch(searchBar: HTMLInputElement) {
+    searchBar.value = '';
+    this.updateSearchTerm('');
+  }
 
   openEditContactModal(contact?: Contact): void {
     this.editContactModal.openModal(contact);
